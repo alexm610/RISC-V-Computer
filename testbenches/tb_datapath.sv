68bdf6795db3cceec60919434e98e3574a3cb514
alexm610 <alexmanak@icloud.com>
2024-07-23 11:36:43 -0700

    alu tests on datapath testbench

--- testbenches/tb_datapath.sv
+++ testbenches/tb_datapath.sv
@@ -6,6 +6,7 @@ module tb_datapath;
     logic [4:0] rs_1, rs_2, rd_0;
     logic [31:0] writedata, alu_result;
     int i = 0;
+    int correct_answer = 0;
 
     datapath dut (.*);
 
@@ -30,12 +31,31 @@ module tb_datapath;
             writedata = (i+1) * 2;
             write_rb = 1; #2;
             write_rb = 0; #2;
-            if (i != 0) begin
+            if (i == 0) begin
+                assert(dut.REGISTER_BANK.readdata_1 == 32'h0) else $error("Register X0 is not hardwired to zero", i);
+            end else begin
                 assert(dut.REGISTER_BANK.readdata_1 == writedata) else $error("Register X%d wasn't written to properly", i);
-            end 
+            end
         end
 
-        #10; 
+        $display("Test 2: operate on registers X23 and X4 - ADD");
+        rs_1 = 32'd4;
+        rs_2 = 32'd23;
+        alu_control = 3'b010;
+        correct_answer = ((4+1) * 2) + ((23+1) * 2);
+        #8; 
+        assert(alu_result == correct_answer) else $error("Addition failed: expected %d, instead got %d", correct_answer, alu_result);
+
+        $display("Test 3: operate on registers X17 and X9 - SUB");
+        rs_1 = 32'd17;
+        rs_2 = 32'd9;
+        alu_control = 3'b110;
+        correct_answer = ((17+1) * 2) - ((9+1) * 2);
+        #8; 
+        assert(alu_result == correct_answer) else $error("Addition failed: expected %d, instead got %d", correct_answer, alu_result);
+
+
+        #4; 
         $stop;
     end 
 
