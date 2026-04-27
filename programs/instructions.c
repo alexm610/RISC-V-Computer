894504a12c5864d0d47ec230814484320ec105c6
alexm610 <alexmanak610@gmail.com>
2026-04-26 22:39:49 -0700

    add performance test

--- programs/instructions.c
+++ programs/instructions.c
@@ -1,4 +1,3 @@
-//#include <stdint.h>
 #include "instructions.h"
 #include "vga.h"
 #include "lcd_display.h"
@@ -7,6 +6,44 @@
 #define BUFFER_MAX_LENGTH   32
 
 extern volatile int timer_ticks;
+
+int a[100][100], b[100][100], c[100][100];
+int i, j, k, sum;
+
+void performance_test(void) {
+    _puts("\n\nStart.....");
+    for(i=0; i <50; i ++)  {
+        _puts("\r\ntick");
+        //_puts("%d ", i);
+        for(j=0; j < 50; j++)  {
+            sum = 0 ;
+            for(k=0; k <50; k++)   {
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+                sum = sum + b[i][k] * b[k][j] + a[i][k] * c[i][j];
+            }
+            c[i][j] = sum ;
+        }
+    }
+    _puts("\n\nDone.....");
+}
 
 int main(void) {
     char *message1 = "Rip and tear,";
@@ -22,21 +59,20 @@ int main(void) {
 
     timer_ticks = 0;
     Timer0_Control_Register = 0x0;
-    Timer0_Data_Register = 0x017D7840;
+    Timer0_Data_Register = 50000000;
     Timer0_Control_Register = 0x00000003;
 
-    _puts("Enter your name: ");
-    FlushKeyboard();
-    _gets(buf, 32);              // blocks until user presses Enter
-    _puts("Hello, ");
-    _puts(buf);
-    _puts("!\n");
-
+    //_puts("Enter your name: ");
+    //FlushKeyboard();
+    //_gets(buf, 32); 
+    //_puts("Hello, ");
+    //_puts(buf);
+    //_puts("!\n");
+    performance_test();
     print_stripes();
     while (1) {
         HEX = timer_ticks;
         LEDR = SWITCHES;
-
-
     }
 }
+
